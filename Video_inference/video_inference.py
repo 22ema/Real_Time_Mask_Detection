1b605cb15bd0f217b0be17c239df59f44cded091
yunsung Han <yunsung9503@gmail.com>
2021-11-06 18:00:17 +0900

    add FPS calc

--- Video_inference/video_inference.py
+++ Video_inference/video_inference.py
@@ -1,5 +1,6 @@
 import cv2
 from inference.inference import *
+import timeit
 
 def video_inference(video_file, weigths, cfg):
     '''
@@ -13,14 +14,16 @@ def video_inference(video_file, weigths, cfg):
     if cap.isOpened():
         while True:
             ret, img = cap.read()   # frame read in video
+            start_t = timeit.default_timer()
             output, trans_image = inference(img, model)  # inference frame
             result_img = post_processing(output, img, trans_image)    # visualize bounding box and class in image
+            terminate_t = timeit.default_timer()
             cv2.imshow('result', result_img)    # display result image
-
+            FPS = int(1./(terminate_t - start_t))
+            print("FPS:", FPS)
             if cv2.waitKey(1) & 0xFF == ord('q'):   # to click keyboard's 'q' button for end
                 break
     else:
         print("can't open video.")
     cap.release()
-    out.release()
     cv2.destroyAllWindows()
